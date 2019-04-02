d8e92304d2295dc3f606cd6c3b6801a9a5cd92b9
sercantor <tor.sercan@gmail.com>
2019-04-02 12:44:12 +0200

    updated entirely

--- unix_prog_proj/mytee.c
+++ unix_prog_proj/mytee.c
@@ -1,10 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
 #include <unistd.h>
 
 
-int main()
+int main(int argc, char* argv[])
 {
+    int fd;
+   
+    if (argc < 2) {
+	fprintf(stderr, "Usage: %s [OPTION]... [FILE]...\n", argv[0]);
+	printf("\t-a\tto open in append mode\n");
+	printf("\t-f\tto truncate\n");
+	exit(1);
+    }
+    
+    /*
+     * if they want to use options
+     */
+    if(argv[1][0] == '-') {
+	switch(argv[1][1]) {
+	    case 'a':
+		printf("they used a, so open in append mode");
+		break;
+	    case 'f':
+		printf("they used f, so use truncate option");
+		break;
+	    default:
+		printf("invalid option\n");
+		exit(1);
+		}
+	    }
+    /*
+     * if not, just create normally
+     */
+    for(int i = 1; i < argc; i++) {
+	fd = open(argv[i], O_WRONLY | O_CREAT, S_IWUSR | S_IRUSR); 
+	    if(fd == -1) {
+		perror("There is error");
+		exit(1);
+	        }
+	/*
+	 * reading/writing from stdin
+	 */
+        char *buffer = (char *)malloc(700*sizeof(char));
+	read(STDIN_FILENO, buffer, 700);
+	write(fd, buffer, 700);
+	write(STDOUT_FILENO, buffer, 700);
+    }
 
-    system("ls -al");
 }
