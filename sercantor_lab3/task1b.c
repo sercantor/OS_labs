def1052426c91285f26510ed5b4d1beb41e0de72
sercantor <tor.sercan@gmail.com>
2019-04-11 08:29:57 +0200

    memory fixed, alloc on heap instad of stack for huge numbers

--- sercantor_lab3/task1b.c
+++ sercantor_lab3/task1b.c
@@ -8,22 +8,23 @@
 #include <time.h>
 
 
-void copy_lib(const char* arg1, const char* arg2)
+void copy_lib(const char* arg1, const char* arg2, const char* argv4)
 {
     FILE *fpin;
     FILE *fpout;
+    long long int size_of_buffer = atoll(argv4);
+    char *array = (char*)malloc(size_of_buffer*sizeof(char));
     fpin = fopen(arg1, "r");
     fpout = fopen(arg2, "w");
-    int c;
-    while((c = fgetc(fpin)) != EOF)
-       fputc(c, fpout);
+    fread(array, size_of_buffer, 1, fpin);
+    fwrite(array,size_of_buffer,1, fpout);
     fclose(fpin);
     fclose(fpout);
 } 
 void copy_system(const char* arg1, const char* arg2, const char* arg4)
 {
-    int size_of_buffer = atoi(arg4);
-    int buf[size_of_buffer];
+    long long int size_of_buffer = atoll(arg4);
+    char *buf = (char*)malloc(size_of_buffer*sizeof(char));
     int fdin;
     int fdout;
     if ((fdin = open(arg1, O_RDONLY)) == -1)
@@ -37,32 +38,18 @@ void copy_system(const char* arg1, const char* arg2, const char* arg4)
         printf("Failed to open write file. Aborting.\n");
         exit(EXIT_FAILURE);
     }
-    while((read(fdin, buf, size_of_buffer)) != 0) {
-	write(fdout, buf, size_of_buffer);
-    }
+
+    read(fdin, buf, size_of_buffer);
+    write(fdout, buf, size_of_buffer);
+    
     close(fdin);
     close(fdout);
 } 
 int main(int argc, const char* argv[])
 {
-    if(strcmp(argv[3], "system") == 0){
-    clock_t t; 
-    t = clock(); 
-    copy_system(argv[1], argv[2], argv[4]);
-    t = clock() - t; 
-    double time_taken = ((double)t)/CLOCKS_PER_SEC; // in seconds 
-  
-    printf("System call copy took %f seconds\n", time_taken); 
-   }
+    if(strcmp(argv[3], "system") == 0)
+	copy_system(argv[1], argv[2], argv[4]);
 
-
-    else if (strcmp(argv[3], "lib") == 0) {
-    clock_t t1; 
-    t1 = clock(); 
-    copy_lib(argv[1], argv[2]);
-    t1 = clock() - t1; 
-    double time_taken1 = ((double)t1)/CLOCKS_PER_SEC; // in seconds 
-  
-    printf("Lib call copy took %f seconds\n", time_taken1); 
-    }
+    else if (strcmp(argv[3], "lib") == 0) 
+	copy_lib(argv[1], argv[2], argv[4]);
 }
