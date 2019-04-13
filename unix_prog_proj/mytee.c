c8a646cadee2b39d4368dd2455bd58f968a60e3e
sercantor <tor.sercan@gmail.com>
2019-04-13 14:45:10 +0200

    deleted string lib, options work now, with pointer manipulation (not really safe)

--- unix_prog_proj/mytee.c
+++ unix_prog_proj/mytee.c
@@ -3,9 +3,25 @@
 #include <fcntl.h>
 #include <errno.h>
 #include <unistd.h>
-#include <string.h>
 
 #define BUFFER_SIZE 4096
+
+void write_to_file(int fd[], int argc)
+{
+
+    char *buf = malloc(BUFFER_SIZE*sizeof(char));
+    int bytes_read;
+    while((bytes_read = read(STDIN_FILENO, buf, sizeof(buf)-1)) > 0) {
+	/*set the last byte to null, this will ensure we won't get random characters*/
+	buf[bytes_read] = 0x00;
+	/*write to stdout*/
+	printf("%s", buf);
+	/*write to file*/
+	for(int i = 0; i < argc; i++) write(fd[i], buf, bytes_read);
+    }
+
+    free(buf);
+} 
 
 int main(int argc, char* argv[])
 {
@@ -20,42 +36,50 @@ int main(int argc, char* argv[])
     
     
     /*if they want to use options*/
-     
     if(argv[1][0] == '-') {
 	switch(argv[1][1]) {
 	    case 'a':
-		printf("they used a, so open in append mode");
+		 for(int i = 2; i < argc; i++) {
+		 fd[i] = open(argv[i], O_WRONLY | O_APPEND | O_CREAT, S_IWUSR | S_IRUSR); 
+		    if(fd[i] == -1) {
+			perror("cannot open file, exiting");
+			exit(1);
+		     } 
+		 }
+		write_to_file(&fd[2],argc);
 		break;
-	    case 'f':
-		printf("they used f, so use truncate option");
+	    case 't':
+		 for(int i = 2; i < argc; i++) {
+		 fd[i] = open(argv[i], O_WRONLY | O_TRUNC | O_CREAT, S_IWUSR | S_IRUSR); 
+		    if(fd[i] == -1) {
+			perror("cannot open file, exiting");
+			exit(1);
+		    } 
+		 }
+		write_to_file(&fd[2],argc);
 		break;
 	    default:
 		printf("invalid option\n");
 		exit(1);
-		}
 	    }
-    /*if not, just create the files, give them individual fd numbers */
-    for(int i = 1; i < argc; i++) {
-	fd[i] = open(argv[i], O_WRONLY | O_CREAT, S_IWUSR | S_IRUSR); 
-	if(fd[i] == -1) perror("cannot open file");
+    }
+    /*if they don't want to use options, just create file(s), error if exists (EXCL)*/
+    else {
+	for(int i = 1; i < argc; i++) {
+	    fd[i] = open(argv[i], O_WRONLY | O_CREAT | O_EXCL, S_IWUSR | S_IRUSR); 
+	    if(fd[i] == -1) {
+		perror("cannot open file, exiting");
+		exit(1);
+	    }
+	} 
+	write_to_file(&fd[1],argc);
     }
 
-    char buf[BUFFER_SIZE];
-    int bytes_read;
 
-    while((bytes_read = read(STDIN_FILENO, buf, sizeof(buf)-1)) > 0) {
-	/*set the last byte to null, this will ensure we won't get random characters*/
-	buf[bytes_read] = 0x00;
-	/*write to stdout*/
-	printf("%s", buf);
-	/*don't write the whole buffer, just write till null*/
-	int nbytes = strlen(buf);
-	for(int i = 1; i < argc; i++) write(fd[i], buf, nbytes);
-    }
-    
     /*after operation is done, close all files*/
     for(int i = 1; i < argc; i++) close(fd[i]);
-     
+
     return 0;
+
 }
 
